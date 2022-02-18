5fbe9e9bc3cd9d4e781f4d3236649ef163f2778e
Xan Johnson <xanjohns@gmail.com>
2022-02-17 19:19:31 -0700

    Only CLBLL_L's

---
+++ or.v
@@ -0,0 +1,16 @@
+module simpleOR(
+    input[1:0] in,  //Input
+    output o_led);  //Output to LED
+
+    //Combinational logic 
+    //
+    //Drives o_led high if in is high
+    always @(*) begin
+        if (in[1] | in[0]) begin
+            o_led <= 1'b1;
+        end else begin
+            o_led <= 1'b0;
+        end
+    end
+
+endmodule
