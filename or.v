5434d0d1f7198b2da661f73ea8f11ec895bc30d5
Joshua Fife <jpfife17@gmail.com>
2022-02-18 11:39:17 -0700

    demonstration of working design with 8 SLICEL's

--- or.v
+++ or.v
@@ -1,16 +1,19 @@
 module simpleOR(
-    input[1:0] in,  //Input
-    output o_led);  //Output to LED
+    input[13:0] in,  //Input
+    output[6:0] o_led);  //Output to LED
 
     //Combinational logic 
     //
     //Drives o_led high if in is high
     always @(*) begin
-        if (in[1] | in[0]) begin
-            o_led <= 1'b1;
-        end else begin
-            o_led <= 1'b0;
-        end
+        o_led[0] = in[0] | in[1];
+        o_led[1] = in[2] | in[3];
+        o_led[2] = in[4] | in[5];
+        o_led[3] = in[6] | in[7];
+        o_led[4] = in[8] | in[9];
+        o_led[5] = in[10] | in[11];
+        o_led[6] = in[12] | in[13];
     end
 
+    // use a width factor of at least 40
 endmodule
