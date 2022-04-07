32780655eb50188066395cda58221da0c0806645
Xan Johnson <xanjohns@gmail.com>
2022-04-07 16:54:11 -0600

    Update length zero

--- length_zero.tcl
+++ length_zero.tcl
@@ -1,8 +1,17 @@
 # Get all signal nets
 set nets [get_nets -filter {TYPE == SIGNAL}]
-set final_list [list ""]
+set final_nets [list]
+set final_list [list]
 foreach net $nets {
   set output_pin [get_pins -of $net -filter {DIRECTION == OUT}]
+  if {$output_pin != ""} {
+    set ref_name [get_property REF_NAME $output_pin]
+  }
+  set lut "LUT"
+  set is_lut [expr {[string first $lut $ref_name] != -1}]
+  if {$is_lut} {
+    set output_pin ""
+  }
   if {$output_pin != ""} {
     set output_site [get_property LOC [get_property PARENT_CELL $output_pin]]
     set input_pins [get_pins -of $net -filter {DIRECTION == IN}]
@@ -11,6 +20,7 @@ foreach net $nets {
         set input_site [get_property LOC [get_property PARENT_CELL $pin]]
         # Add to list if input pin site and output pin site are equal
         if {$input_site == $output_site} {
+          lappend final_nets $net
           set input_BEL [get_property BEL [get_property PARENT_CELL $pin]]
           set output_BEL [get_property BEL [get_property PARENT_CELL $output_pin]]
           set temp_string "$input_site:$input_BEL to $output_BEL \n"
@@ -20,4 +30,7 @@ foreach net $nets {
     }
   }
 }
-puts [lsort -dictionary $final_list]
+#puts [lsort -dictionary $final_list]
+puts [lsort -dictionary $final_nets]
+
+select_objects $final_nets
